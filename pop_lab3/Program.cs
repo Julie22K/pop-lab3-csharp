50a9bd25464521c10659a37a7d67d160d23e439a
Julie22K <yuliakononenko22@gmail.com>
2023-05-12 08:49:49 +0300

    edited

--- pop_lab3/Program.cs
+++ pop_lab3/Program.cs
@@ -12,7 +12,7 @@ namespace pop_lab3
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.Starter(3, 10,4,3);
+            program.Starter(3, 10,6,3);
 
             Console.ReadKey();
         }
@@ -28,24 +28,24 @@ namespace pop_lab3
             for (int i = 0; i < consumer_num; i++)
             { 
                 Thread threadConsumer = new Thread(Consumer);
-                threadConsumer.Name = "Consumer#" + i;
+                threadConsumer.Name = "Consumer#" + (i+1);
                 threadConsumer.Start(itemNumbers);
             }
             for (int i = 0; i < producers_num; i++)
             {
                 Thread threadProducer = new Thread(Producer);
-                threadProducer.Name = "Producer#" + i;
+                threadProducer.Name = "Producer#" + (i+1);
                 threadProducer.Start(itemNumbers);
             }
         }
-
         private Semaphore Access;
 
         private Semaphore Full;
         private Semaphore Empty;
 
-        private readonly List<string> storage = new List<string>();
-        private int counter=0;
+
+        private int num_of_last_item=0;
+        private List<string> storage= new List<string>();
         private void Producer(Object itemNumbers)
         {
             int maxItem = 0;
@@ -53,22 +53,26 @@ namespace pop_lab3
             {
                 maxItem = (int)itemNumbers;
             }
-            
-            while (counter < maxItem)
+
+            for (int i = 0; i < maxItem; i++)
             {
                 Full.WaitOne();
                 Access.WaitOne();
-                if (counter == maxItem) {
-                    Full.Release();
+                if (num_of_last_item < maxItem)
+                {
+                    storage.Add("item " + num_of_last_item);
+                    Console.WriteLine(Thread.CurrentThread.Name + " added item " + num_of_last_item);
+                    num_of_last_item++;
+
+                    Empty.Release();
+                    Access.Release();
+                }
+                else
+                {
                     Access.Release();
                     break;
-                }
-                storage.Add("item " + counter);
-                Console.WriteLine(Thread.CurrentThread.Name+" added item " + counter);
-                counter++;
 
-                Access.Release();
-                Empty.Release();
+                }
 
             }
         }
@@ -80,22 +84,21 @@ namespace pop_lab3
             {
                 maxItem = (int)itemNumbers;
             }
-            while (storage.Count>0)
+            for (int i = 0; i < maxItem; i++)
             {
+                if (num_of_last_item == maxItem) break;
                 Empty.WaitOne();
                 Thread.Sleep(1000);
                 Access.WaitOne();
 
                 string item = storage.ElementAt(0);
                 storage.RemoveAt(0);
+                Console.WriteLine(Thread.CurrentThread.Name + " took " + item);
 
                 Full.Release();
-
                 Access.Release();
 
-                Console.WriteLine(Thread.CurrentThread.Name + " took " + item);
             }
         }
     }
 }
-s
